de41269dff802d092f02953f3e065db68265b020
kimagin <info@kimagination.net>
2024-07-10 01:04:55 +0200

    modification on lualine style

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,13 +10,14 @@ return {
         theme = "auto",
         globalstatus = true,
         disabled_filetypes = { statusline = { "dashboard", "alpha" } },
-        component_separators = "",
-        section_separators = "",
+        component_separators = "|",
+        section_separators = "--",
       },
       sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {
           {
             "diagnostics",
             symbols = {
@@ -26,37 +27,12 @@ return {
               hint = icons.diagnostics.Hint,
             },
           },
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+          { "filetype", icon_only = true, separator = "", padding = { left = 2, right = 1 } },
+          "mode",
         },
-        lualine_x = {
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-            color = Util.ui.fg("Statement"),
-          },
-          -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = Util.ui.fg("Constant"),
-          },
-          {
-            require("lazy.status").updates,
-            cond = require("lazy.status").has_updates,
-            color = Util.ui.fg("Special"),
-          },
-        },
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          function()
-            return " " .. os.date("%R")
-          end,
-        },
+        lualine_y = {},
+
+        lualine_z = {},
       },
       extensions = { "neo-tree", "lazy" },
     }
