5d24894fc7a2d3f0c94c4d55790f4da6ca037e4d
kimagin <info@kimagination.net>
2024-07-09 22:56:35 +0200

    junks

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,10 +4,18 @@ return {
     opts = function(_, opts)
       local cmp = require("cmp")
 
+      opts.completion = vim.tbl_extend("force", opts.completion or {}, {
+        timeout = 300, -- Adjust this value (in milliseconds) as needed
+      })
+      opts.performance = {
+        debounce = 300,
+        throttle = 60,
+        fetching_timeout = 200,
+      }
       opts.window = {
         completion = {
           border = "single",
-          winhighlight = "Normal:Normal,FloatBorder:TelescopeBorder,CursorLine:@comment.todo,Search:Pmenu",
+          winhighlight = "Normal:Normal,FloatBorder:TelescopeBorder,CursorLine:MiniDepsTitleSame,Search:Pmenu",
         },
         documentation = {
           border = "single",
