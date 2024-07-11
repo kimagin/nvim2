127ef58777840a7ecbe21cab159e37de409baab2
kimagin <info@kimagination.net>
2024-07-11 12:39:07 +0200

    including certain file types for markdown

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,45 @@
+return {
+
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = {} },
+  },
+  -- lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {},
+    },
+  },
+  -- MarkdownPreview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+
+      formatters_by_ft = {
+        ["markdown"] = { "prettierd" },
+        ["markdown_inline"] = { "prettierd" },
+      },
+    },
+  },
+}
