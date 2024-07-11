3e85c25f32ce482eaa4048b6b2ffca7ab943d237
kimagin <info@kimagination.net>
2024-07-11 12:36:27 +0200

    attempt for disabeling snippets

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -21,6 +21,7 @@ return {
   -- { "hrsh7th/cmp-nvim-lsp", enabled = false },
   -- { "hrsh7th/cmp-buffer", enabled = false },
   -- { "hrsh7th/cmp-path", enabled = false },
+  -- { "garymjr/nvim-snippets", enabled = false },
   -- { "saadparwaiz1/cmp_luasnip", enabled = false },
   -- { "L3MON4D3/LuaSnip", enabled = false },
   -- { "rafamadriz/friendly-snippets", enabled = false },
