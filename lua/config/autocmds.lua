fd64a8e1de5942c8938f1b5a630557ddc7d52037
kimagin <info@kimagination.net>
2024-07-12 20:02:45 +0200

    adding new hlgroups

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -31,6 +31,11 @@ vim.api.nvim_set_hl(0, "markdownHeadingDelimiter", { fg = "#A88BFA" })
 vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#2f3031" })
 
 vim.api.nvim_set_hl(0, "MiniDepsTitleSame", { fg = "#A88BFA", bg = "#211a33" })
+vim.api.nvim_set_hl(0, "@markup.list.checked", { bold = true, fg = "#A88BFA" })
+vim.api.nvim_set_hl(0, "@markup.list.unchecked", { bold = true, fg = "#A88BFA" })
+vim.api.nvim_set_hl(0, "@markup.quote", { bold = false, fg = "#C6D0F6", bg = "#0F1014" })
+vim.api.nvim_set_hl(0, "markdownCodeBlock", { fg = "#a6aef8" })
+vim.api.nvim_set_hl(0, "markdownCode", { fg = "#a6aef2", blend = 10 })
 
 -- Yaml
 
@@ -38,3 +43,35 @@ vim.api.nvim_set_hl(0, "yamlKeyValueDelimiter", { fg = "#A88BFA" })
 vim.api.nvim_set_hl(0, "yamlDocumentStart", { fg = "#A88BFA" })
 
 vim.api.nvim_set_hl(0, "yamlString", { fg = "#C6D0F6" })
+
+-- Telekasten
+-- Launch panel if nothing is typed after <leader>z
+vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
+
+-- Most used functions
+vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
+vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
+vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
+vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
+vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
+vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
+vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
+vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
+
+-- Adding strikethrough to the completed tasks
+vim.api.nvim_create_augroup("MarkdownTaskListDone", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = "MarkdownTaskListDone",
+  pattern = "markdown",
+  callback = function()
+    -- Match completed tasks: '- [x]', '* [x]', or any number of spaces before these
+    vim.cmd([[syntax match markdownTaskListDone /^\s*[-*]\s\[x\].*$/]])
+
+    -- Set highlight with strikethrough
+    vim.api.nvim_set_hl(0, "markdownTaskListDone", { fg = "#A88BFA", strikethrough = true, italic = true })
+
+    -- Link the syntax match to the highlight group
+    vim.cmd([[highlight link markdownTaskListDone markdownTaskListDone]])
+  end,
+})
