a4928c7e89c924d116315524b032b5ffbbd0370a
kimagin <info@kimagination.net>
2024-07-13 02:38:17 +0200

    removing keys for an old plugin

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -44,20 +44,6 @@ vim.api.nvim_set_hl(0, "yamlDocumentStart", { fg = "#A88BFA" })
 
 vim.api.nvim_set_hl(0, "yamlString", { fg = "#C6D0F6" })
 
--- Telekasten
--- Launch panel if nothing is typed after <leader>z
-vim.keymap.set("n", "<leader>z", "<cmd>Telekasten panel<CR>")
-
--- Most used functions
-vim.keymap.set("n", "<leader>zf", "<cmd>Telekasten find_notes<CR>")
-vim.keymap.set("n", "<leader>zg", "<cmd>Telekasten search_notes<CR>")
-vim.keymap.set("n", "<leader>zd", "<cmd>Telekasten goto_today<CR>")
-vim.keymap.set("n", "<leader>zz", "<cmd>Telekasten follow_link<CR>")
-vim.keymap.set("n", "<leader>zn", "<cmd>Telekasten new_note<CR>")
-vim.keymap.set("n", "<leader>zc", "<cmd>Telekasten show_calendar<CR>")
-vim.keymap.set("n", "<leader>zb", "<cmd>Telekasten show_backlinks<CR>")
-vim.keymap.set("n", "<leader>zI", "<cmd>Telekasten insert_img_link<CR>")
-
 -- Adding strikethrough to the completed tasks
 vim.api.nvim_create_augroup("MarkdownTaskListDone", { clear = true })
 
