8032410be0cec8931f0038d75659724a08d61d80
kimagin <info@kimagination.net>
2024-07-13 16:48:13 +0200

    replacing fzf-lua with fzf-native for better performance

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,17 @@
 return {
   {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- configure fzf-lua here if needed
+    end,
+  },
+  {
     "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
+    opts = function(_, opts)
+      local fzf_lua = require("fzf-lua")
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
         layout_config = {
           horizontal = {
             prompt_position = "bottom",
@@ -19,7 +28,31 @@ return {
         winblend = 0,
         border = true,
         borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-      },
+        -- FZF-lua specific configurations
+        fuzzy = true,
+        vimgrep_arguments = fzf_lua.defaults.grep.cmd,
+      })
+      opts.pickers = vim.tbl_deep_extend("force", opts.pickers or {}, {
+        find_files = {
+          find_command = fzf_lua.defaults.files.cmd,
+        },
+      })
+      -- Use fzf-lua for the fuzzy finding
+      opts.extensions = vim.tbl_deep_extend("force", opts.extensions or {}, {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      })
+      return opts
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "ibhagwan/fzf-lua",
     },
   },
+  -- Optionally disable fzf-native if you're not using it
+  { "nvim-telescope/telescope-fzf-native.nvim", enabled = false },
 }
