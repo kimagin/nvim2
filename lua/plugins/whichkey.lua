a975c861df69f9d58805fc8c657964de8ba19aa7
kimagin <info@kimagination.net>
2024-07-13 02:38:42 +0200

    reverting plugin after update to it's initial settings

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -2,41 +2,32 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    plugins = { spelling = {
-      enabled = true,
-      suggestions = 20,
-    } },
-    defaults = {
-      mode = { "n", "v" },
-      ["g"] = { name = "+goto" },
-      ["gs"] = { name = "+surround" },
-      ["z"] = { name = "+fold" },
-      ["]"] = { name = "+next" },
-      ["["] = { name = "+prev" },
-      ["<leader><tab>"] = { name = "+tabs" },
-      ["<leader>b"] = { name = "+buffer" },
-      ["<leader>c"] = { name = "+code" },
-      ["<leader>f"] = { name = "+file/find" },
-      ["<leader>g"] = { name = "+git" },
-      ["<leader>gh"] = { name = "+hunks", ["_"] = "which_key_ignore" },
-      ["<leader>q"] = { name = "+quit/session" },
-      ["<leader>s"] = { name = "+search" },
-      ["<leader>u"] = { name = "+ui" },
-      ["<leader>w"] = { name = "+windows" },
-      ["<leader>x"] = { name = "+diagnostics/quickfix" },
+    plugins = {
+      spelling = {
+        enabled = true,
+        suggestions = 20,
+      },
     },
-    window = {
-      border = "single", -- none, single, double, shadow
-      position = "bottom", -- bottom, top
-      margin = { 0, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
-      padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-      winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
-      zindex = 1000, -- positive value to position WhichKey above other floating windows.
+    win = {
+      -- width = 1,
+      -- height = { min = 4, max = 25 },
+      -- col = 0,
+      row = -1,
+      border = "single",
+      padding = { 1, 2 }, -- extra window padding [top/bottom, right/left]
+      title = false,
+      -- title_pos = "center",
+      zindex = 1000,
+      -- Additional vim.wo and vim.bo options
+      bo = {},
+      wo = {
+        winblend = 10, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+      },
+    },
+    icons = {
+      enabled = false,
+      colors = false,
+      rules = false,
     },
   },
-  config = function(_, opts)
-    local wk = require("which-key")
-    wk.setup(opts)
-    wk.register(opts.defaults)
-  end,
 }
