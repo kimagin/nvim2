cefde1420641f417751e55a0605fa11daa02bc03
kimagin <info@kimagination.net>
2024-07-15 01:59:50 +0200

    fix the root issue crashing and organize telescope windows

--- lua/plugins/telescope-plugs.lua
+++ lua/plugins/telescope-plugs.lua
@@ -6,6 +6,66 @@ return {
     },
     keys = {
       {
+        "<leader><leader>",
+        function()
+          local function find_project_root()
+            local current_file = vim.fn.expand("%:p")
+            local current_dir = vim.fn.fnamemodify(current_file, ":h")
+            return vim.fn.finddir(".git/..", current_dir .. ";")
+          end
+
+          local root = find_project_root()
+          if root == "" then
+            root = vim.fn.getcwd()
+          end
+
+          require("telescope.builtin").find_files({
+            cwd = root,
+            hidden = true,
+            no_ignore = false,
+            find_command = {
+              "fd",
+              "--type",
+              "f",
+              "--hidden",
+              "--follow",
+              "--exclude",
+              ".git",
+              "--exclude",
+              "node_modules",
+              "--exclude",
+              "dist",
+              "--exclude",
+              ".local",
+              "--exclude",
+              ".Trash",
+              "--exclude",
+              ".vscode",
+              "--exclude",
+              ".tldrc",
+              "--exclude",
+              "Library",
+              "--exclude",
+              ".cache",
+              "--exclude",
+              ".vscode-server",
+              "--exclude",
+              ".npm",
+              "--exclude",
+              ".pnpm",
+              "--exclude",
+              ".continue",
+              "--exclude",
+              ".ollama",
+              "--exclude",
+              "OneDrive",
+            },
+          })
+        end,
+        desc = "Find files from project root",
+      },
+
+      {
         "<leader>fg",
         function()
           local pickers = require("telescope.pickers")
@@ -13,7 +73,6 @@ return {
           local conf = require("telescope.config").values
           local actions = require("telescope.actions")
           local action_state = require("telescope.actions.state")
-
           local function get_git_repos()
             local search_dir = os.getenv("HOME")
             local cmd = string.format(
@@ -29,17 +88,26 @@ return {
             end
             return repos
           end
-
           pickers
-            .new({}, {
+            .new({
+              layout_strategy = "vertical",
+
+              layout_config = {
+                width = 0.6,
+                height = 0.5,
+                prompt_position = "bottom",
+              },
+            }, {
               prompt_title = "Git Repositories",
+
               finder = finders.new_table({
                 results = get_git_repos(),
                 entry_maker = function(entry)
+                  local repo_name = entry:match("([^/]+)$")
                   return {
                     value = entry,
-                    display = entry,
-                    ordinal = entry,
+                    display = repo_name,
+                    ordinal = repo_name,
                   }
                 end,
               }),
