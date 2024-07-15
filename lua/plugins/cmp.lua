639fccc5edaec2cdd626e2f81d182ee750df9ed5
kimagin <info@kimagination.net>
2024-07-15 12:53:45 +0200

    adding key bindings to cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,6 +18,13 @@ return {
           winhighlight = "Normal:Normal,FloatBorder:TelescopeBorder,CursorLine:@comment.todo,Search:Pmenu",
         },
       }
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Add Enter key for confirming selection
+      })
     end,
   },
 }
