1d5d30f42c5fa9c485905d3f149b5470c2e71e2d
kimagin <info@kimagination.net>
2024-07-16 13:58:46 +0200

    remove animations from the notifications

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,33 +2,13 @@ return {
   "rcarriga/nvim-notify",
   enabled = true,
   opts = {
-    background_colour = "#000000",
-    fps = 60,
-    icons = {
-      DEBUG = "",
-      ERROR = "",
-      INFO = "",
-      TRACE = "✎",
-      WARN = "",
-    },
+    -- background_colour = "#000000",
+    fps = 1,
     level = 2,
     minimum_width = 50,
     render = "minimal",
-    stages = "fade",
+    stages = "static",
     timeout = 3000,
     top_down = true,
   },
-  config = function(_, opts)
-    local notify = require("notify")
-    notify.setup(opts)
-    -- Override the default notification function
-    vim.notify = function(msg, level, notif_opts)
-      notif_opts = notif_opts or {}
-      notif_opts.on_open = function(win)
-        local buf = vim.api.nvim_win_get_buf(win)
-        vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
-      end
-      return notify(msg, level, notif_opts)
-    end
-  end,
 }
