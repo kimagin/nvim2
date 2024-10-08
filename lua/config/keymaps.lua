161e41a8528ba91afdbd0788e74749c376948419
kimagin <info@kimagination.net>
2024-10-08 06:31:00 +0200

    fixing buffer hopping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,45 +3,39 @@
 -- Add any additional keymaps here
 ---@diagnostic disable: undefined-global
 
-local function cycle_buffers(direction)
-  local bufs = vim.tbl_filter(function(buf)
-    return vim.bo[buf].buflisted and vim.api.nvim_buf_is_loaded(buf)
+local function get_listed_buffers()
+  return vim.tbl_filter(function(bufnr)
+    return vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].buflisted
   end, vim.api.nvim_list_bufs())
-
-  if #bufs < 2 then
-    return
-  end
-
-  local cur = vim.api.nvim_get_current_buf()
-  local idx = (function()
-    for i, v in ipairs(bufs) do
-      if v == cur then
-        return i
-      end
-    end
-  end)()
-
-  if not idx then
-    return
-  end
-
-  if direction == "next" then
-    idx = idx % #bufs + 1
-  else
-    idx = (idx - 2) % #bufs + 1
-  end
-
-  vim.api.nvim_set_current_buf(bufs[idx])
 end
 
--- Window action Keymaps
-vim.keymap.set("n", "<leader>bn", function()
-  cycle_buffers("next")
-end, { desc = "Next buffer" })
+local function switch_buffer(direction)
+  local buffers = get_listed_buffers()
+  if #buffers < 2 then
+    return
+  end
 
+  local current = vim.api.nvim_get_current_buf()
+  local index = vim.tbl_contains(buffers, current) and vim.fn.index(buffers, current) or 0
+
+  if direction == "next" then
+    index = (index + 1) % #buffers
+  elseif direction == "prev" then
+    index = (index - 1 + #buffers) % #buffers
+  end
+
+  vim.api.nvim_set_current_buf(buffers[index + 1])
+end
+
+vim.keymap.set("n", "<leader>bn", function()
+  switch_buffer("next")
+end, { desc = "Next buffer" })
 vim.keymap.set("n", "<leader>bb", function()
-  cycle_buffers("prev")
+  switch_buffer("prev")
 end, { desc = "Previous buffer" })
+
+-- Use Telescope for buffer picking
+vim.keymap.set("n", "<leader>bl", "<cmd>Telescope buffers<cr>", { desc = "Switch Buffer" })
 
 vim.keymap.set("n", "<leader>qq", "<cmd>wa | bdelete<CR>", { desc = "Save and close buffer" })
 vim.keymap.set("n", "<leader>ww", "<cmd>w<CR>", { desc = "Save current buffer" })
@@ -50,9 +44,11 @@ vim.keymap.set("n", "Y", "y$", { desc = "Yank to end of line" })
 
 --Floating terminal
 
-local lazyterm = function()
-  LazyVim.terminal(nil, {
-    cwd = LazyVim.root(),
+local Util = require("lazyvim.util")
+
+local function lazyterm()
+  Util.terminal(nil, {
+    cwd = Util.root(),
     border = "single",
     highlights = { border = "TelescopeBorder" },
     title = "-|   Terminal   |-",
@@ -64,10 +60,9 @@ end
 
 vim.keymap.set("n", "<leader>ft", lazyterm, { desc = "Terminal (Root Dir)" })
 vim.keymap.set("n", "<leader>fT", function()
-  LazyVim.terminal()
+  Util.terminal()
 end, { desc = "Terminal (cwd)" })
 vim.keymap.set("n", "<c-\\>", lazyterm, { desc = "Terminal (Root Dir)" })
-vim.keymap.set("n", "<c-_>", lazyterm, { desc = "which_key_ignore" })
 
 -- Terminal Mappings
 vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
@@ -76,4 +71,5 @@ vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<cr>", { desc = "Go to Lower Window" 
 vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<cr>", { desc = "Go to Upper Window" })
 vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<cr>", { desc = "Go to Right Window" })
 vim.keymap.set("t", "<C-\\>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-vim.keymap.set("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
+
+-- Closing all other buffers
