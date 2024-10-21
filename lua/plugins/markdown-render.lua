4a7829a5a453b8d8978a27a640630741b7688a94
kimagin <info@kimagination.net>
2024-10-21 06:52:07 +0200

    remove the space on the checkbox

--- lua/plugins/markdown-render.lua
+++ lua/plugins/markdown-render.lua
@@ -197,7 +197,7 @@ return {
       position = "inline",
       unchecked = {
         -- Replaces '[ ]' of 'task_list_marker_unchecked'
-        icon = "󰄱 ",
+        icon = "󰄱",
         -- Highlight for the unchecked icon
         highlight = "RenderMarkdownUnchecked",
         -- Highlight for item associated with unchecked checkbox
@@ -205,7 +205,7 @@ return {
       },
       checked = {
         -- Replaces '[x]' of 'task_list_marker_checked'
-        icon = "󰄲 ",
+        icon = "󰄲",
         -- Highligh for the checked icon
         highlight = "RenderMarkdownChecked",
         -- Highlight for item associated with checked checkbox
