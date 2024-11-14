7b19b1797afacfd0932523f3672b1e4c514149e2
kimagin <info@kimagination.net>
2024-11-14 13:28:14 +0100

    replaceing codeium with supermaven

--- lua/plugins/codium.lua
+++ lua/plugins/codium.lua
@@ -1,36 +1,36 @@
 return {
-  "Exafunction/codeium.vim",
-  event = "BufEnter",
-  config = function()
-    -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set("i", "<c-g>", function()
-      return vim.fn["codeium#Accept"]()
-    end, { expr = true, silent = true })
-    vim.keymap.set("i", "<C-]>", function()
-      return vim.fn["codeium#CycleCompletions"](1)
-    end, { expr = true, silent = true })
-    vim.keymap.set("i", "<C-}>", function()
-      return vim.fn["codeium#CycleCompletions"](-1)
-    end, { expr = true, silent = true })
-    vim.keymap.set("i", "<c-x>", function()
-      return vim.fn["codeium#Clear"]()
-    end, { expr = true, silent = true })
-
-    vim.g.codeium_disable_bindings = 1
-    vim.g.codeium_idle_delay = 150
-
-    -- Manual request
-    -- vim.keymap.set("i", "<c-]>", function()
-    --   return vim.fn["codeium#Complete"]()
-    -- end, { expr = true, silent = true })
-
-    vim.g.codeium_filetypes = {
-      ["*"] = true,
-      ["markdown"] = true,
-      -- Add more filetype-specific settings as needed
-    }
-
-    -- API kEY
-    vim.g.codeium_api_key = os.getenv("CODEIUM")
-  end,
+  -- "Exafunction/codeium.vim",
+  -- event = "BufEnter",
+  -- config = function()
+  --   -- Change '<C-g>' here to any keycode you like.
+  --   vim.keymap.set("i", "<c-g>", function()
+  --     return vim.fn["codeium#Accept"]()
+  --   end, { expr = true, silent = true })
+  --   vim.keymap.set("i", "<C-]>", function()
+  --     return vim.fn["codeium#CycleCompletions"](1)
+  --   end, { expr = true, silent = true })
+  --   vim.keymap.set("i", "<C-}>", function()
+  --     return vim.fn["codeium#CycleCompletions"](-1)
+  --   end, { expr = true, silent = true })
+  --   vim.keymap.set("i", "<c-x>", function()
+  --     return vim.fn["codeium#Clear"]()
+  --   end, { expr = true, silent = true })
+  --
+  --   vim.g.codeium_disable_bindings = 1
+  --   vim.g.codeium_idle_delay = 150
+  --
+  --   -- Manual request
+  --   -- vim.keymap.set("i", "<c-]>", function()
+  --   --   return vim.fn["codeium#Complete"]()
+  --   -- end, { expr = true, silent = true })
+  --
+  --   vim.g.codeium_filetypes = {
+  --     ["*"] = true,
+  --     ["markdown"] = true,
+  --     -- Add more filetype-specific settings as needed
+  --   }
+  --
+  --   -- API kEY
+  --   vim.g.codeium_api_key = os.getenv("CODEIUM")
+  -- end,
 }
