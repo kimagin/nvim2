6b5d902ec71f5d63a0da2baddcbefee92a032d12
kimagin <info@kimagination.net>
2024-11-20 17:05:29 +0100

    adding mini operators

---
+++ lua/plugins/operators.lua
@@ -0,0 +1,35 @@
+return {
+  "echasnovski/mini.operators",
+  event = "VeryLazy",
+  config = function()
+    require("mini.operators").setup({
+      -- add operators that will trigger motion and text object completion
+      -- to enable all native operators, set the table below to {}
+      operators = {
+        gc = "Comments",
+        gb = "Blocks",
+        ge = "End",
+        gE = "End",
+        d = "Delete",
+        c = "Change",
+        y = "Yank",
+        p = "Put",
+        r = "Replace",
+        ["."] = "RepeatLastChange",
+      },
+      -- add operators that will on visual mode startup
+      on_startup = {
+        gc = "Comments",
+        gb = "Blocks",
+        ge = "End",
+        gE = "End",
+        d = "Delete",
+        c = "Change",
+        y = "Yank",
+        p = "Put",
+        r = "Replace",
+        ["."] = "RepeatLastChange",
+      },
+    })
+  end,
+}
